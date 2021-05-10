f16bd8dbf9b32d4f58b3ead0d1788acd6e92793a
Duotech Academy <duotechinstructor@gmail.com>
2021-05-10 19:28:26 -0400

    added another test

--- src/test/java/tests/FatihTest.java
+++ src/test/java/tests/FatihTest.java
@@ -3,7 +3,7 @@ package tests;
 public class FatihTest {
     public static void main(String[] args) {
 
-        System.out.println("Hi Team, We need to do meeting soon.");
+        System.out.println("I am changing this line and MY change is correct");
     }
 
 }
