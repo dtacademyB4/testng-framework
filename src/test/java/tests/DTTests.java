24870bd068fe3ebfd1c962bb69e1e94f9ec6a1d2
Duotech Academy <duotechinstructor@gmail.com>
2021-05-10 19:17:48 -0400

    added another test

--- src/test/java/tests/DTTests.java
+++ src/test/java/tests/DTTests.java
@@ -9,4 +9,10 @@ public class DTTests {
     public void test(){
         System.out.println("Added my changes");
     }
+
+
+    @Test
+    public void test2(){
+        System.out.println("Second");
+    }
 }
